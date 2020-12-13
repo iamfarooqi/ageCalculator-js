591e5bc6cb96c293d863dbd5ca7d2deef44b076d
FAROOQI <attaurrahmanfarooqi9@gmail.com>
2020-12-13 18:23:15 +0500

    1

---
+++ app.js
@@ -0,0 +1,27 @@
+const ageChecker = ()=>{
+
+    setInterval(function(){
+        var dob = document.getElementById("dob").value;
+
+var date  = new Date(dob);
+//    var date  = new Date("4 april,1994");
+ var timeFixing = new Date();
+
+var  fixingMili = timeFixing.getTime();
+
+ dateMili = date.getTime();
+ 
+ diff = fixingMili - dateMili; 
+
+ myDob = Math.floor (diff/(1000*60*60*24*30*12));
+ month = Math.floor (diff/(1000*60*60*24*30));
+ days = Math.floor (diff/(1000*60*60*24));
+ hours = Math.floor (diff/(1000*60*60));
+ minutes = Math.floor (diff/(1000*60));
+ seconds = Math.floor (diff/(1000));
+ smallest = Math.floor (diff);
+document.getElementById("yourage").innerHTML = ` <b>Age</b>: ${myDob} <br> <b>Month:</b>:${month} <br> <b>Days:</b>:${days} <br>  <b>Hours:</b>:${hours} <br> <b>Minutes:</b>:${minutes} <br> <b>Seconds:</b>:${seconds} <br> <b>Milliseconds:</b> ${smallest}`
+
+    },1000)
+   return false;
+}
